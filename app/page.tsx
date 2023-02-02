58455d287bb953a29221e1913c6d65fb950f6580
Filip Chrapek <hello@filipchrapek.com>
2023-02-02 21:21:26 +0100

    Add search input

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
-// import { useState } from "react";
-import BlogSection from '@/components/BlogSection';
+"use client";
+import { useState } from "react";
+import { BlogSection, Container } from '@/components';
 import { allPosts } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
+
 // import { compareDesc } from "date-fns";
 
 export default function BlogIndexPage() {
-  const posts = allPosts.filter((post) => post.published)
+  // const posts = allPosts.filter((post) => post.published)
+
+  const [searchValue, setSearchValue] = useState("");
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    })
+    .filter((post) => {
+      if (searchValue === "") {
+        return post;
+      } else if (
+        post.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+        post.description.toLowerCase().includes(searchValue.toLowerCase())
+      ) {
+        return post;
+      }
+    });
 
   return (
-    <BlogSection posts={posts} />
+    <>
+      <Container >
+        <input
+          type="text"
+          name="search"
+          id="search"
+          onChange={(e) => setSearchValue(e.target.value)}
+          aria-label="Wyszukaj artykuł"
+          placeholder="Wyszukaj artykuł..."
+          className=""
+        />
+      </Container>
+
+      <BlogSection posts={posts} />
+    </>
   );
 }
