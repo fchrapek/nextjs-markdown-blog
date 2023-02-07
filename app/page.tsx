877c703cbbf1689174404178e840eda2a4e58129
Filip Chrapek <hello@filipchrapek.com>
2023-02-07 17:28:52 +0100

    Add new article draft

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import { compareDesc } from "date-fns";
 import styles from './page.module.scss';
 
 export default function BlogIndexPage() {
-  // const posts = allPosts.filter((post) => post.published)
-
   const [searchValue, setSearchValue] = useState("");
   const posts = allPosts
     .filter((post) => post.published)
